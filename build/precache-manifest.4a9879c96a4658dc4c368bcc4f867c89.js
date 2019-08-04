9d9e2ffc043b857257ce3d6a004b4664e00ff00a
Tiago Machado <tiagodjm@gmail.com>
2019-08-05 03:46:02 +1000

    Adding Robofriends project

---
+++ build/precache-manifest.4a9879c96a4658dc4c368bcc4f867c89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba87f64c3301bb2cf3d4216a4a7296d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7b410ad1c35305828ff0",
+    "url": "/static/css/main.24fe45e4.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "7b410ad1c35305828ff0",
+    "url": "/static/js/main.71ce987d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
